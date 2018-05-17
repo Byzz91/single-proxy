5d7774b5b723194396351c922f425d66a6366120
byzz <eyedroot@gmail.com>
2018-05-17 11:48:09 +0900

    update for local

--- iproxy.js
+++ iproxy.js
@@ -1,4 +1,5 @@
 let http        = require('http')
+    , ip        = require('ip')
     , moment    = require('moment')
     , url       = require('url')
     , httpProxy = require('http-proxy')
@@ -8,56 +9,37 @@ let http        = require('http')
     , os        = require('os')
     , workers   = [];
 
-const HOST_IP = '10.50.140.174';
+const HOST_IP = ip.address();
 const LISTEN_PORT = 8722;
+const VERSION = '1.0';
 
-if (cluster.isMaster) {
-    console.log('iProxy was started...');
-    console.log(`> ${HOST_IP}:${LISTEN_PORT}`);
+proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    var target = url.parse(req.url);
 
-    // create workers
-    for (var i=0, length=os.cpus().length; i < length; i++) {
-        workers.push(cluster.fork());
+    if (/^(?:(?!adn|ads|static|upload2)[\w\d\-]+)\.inven\.co\.kr$/i.test(target.host)) {
+        proxyReq.setHeader('X-Special-Inven-Header', `iProxy@${VERSION} <${process.env.PROXY_USER}>`);
+    }
+});
+
+proxy.on('error', function (err, req, res) {
+    if (!res.headersSent) {
+        res.writeHead(500);
     }
 
-    cluster.on('exit', function (worker, code, signal) {
-        workers.push(cluster.fork());
-        console.log('worker was died. restarted worker');
+    res.end();
+});
+
+http.createServer(function(req, res) {
+    var target = url.parse(req.url)
+        , originHost = target.host;
+
+    if (/^(?:(?!adn|ads|static|upload2|kstatic|img)[\w\d\-]+)\.inven\.co\.kr$/i.test(target.host)) {
+        target.host = process.env.MIRROR_HOST;
+    }
+
+    console.log('[%s] forwarded %s//%s to %s//%s', moment().format('YYYY-MM-DD HH:mm:ss'), target.protocol, originHost, target.protocol, target.host);
+
+    proxy.web(req, res, {
+        target: target.protocol + '//' + target.host
     });
-
-    cluster.on('SIGINT', function () {
-        console.log('\nshutting down ...');
-        process.exit(0);
-    });
-} else { // Proxy Logic
-    proxy.on('proxyReq', function (proxyReq, req, res, options) {
-        var target = url.parse(req.url);
-
-        if (/^(?:(?!adn|ads|static|upload2)[\w\d\-]+)\.inven\.co\.kr$/i.test(target.host)) {
-            proxyReq.setHeader('X-Special-Inven-Header', process.env.PROXY_USER.toLowerCase() + ':' + process.env.DB_MODE.toLowerCase());
-        }
-    });
-
-    proxy.on('error', function (err, req, res) {
-        if (!res.headersSent) {
-            res.writeHead(500);
-        }
-
-        res.end();
-    });
-
-    http.createServer(function(req, res) {
-        var target = url.parse(req.url)
-            , originHost = target.host;
-
-        if (/^(?:(?!adn|ads|static|upload2|kstatic|img)[\w\d\-]+)\.inven\.co\.kr$/i.test(target.host)) {
-            target.host = process.env.PROXY_HOST;
-        }
-
-        console.log('[%s] forwarded %s//%s to %s//%s', moment().format('YYYY-MM-DD HH:mm:ss'), target.protocol, originHost, target.protocol, target.host);
-
-        proxy.web(req, res, {
-            target: target.protocol + '//' + target.host
-        });
-    }).listen(LISTEN_PORT, HOST_IP);
-}
+}).listen(LISTEN_PORT, HOST_IP);
