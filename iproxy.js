29e7546043d3d048071e2f7d6c65f7647b994add
byzz <byzz@inven.co.kr>
2018-06-26 15:03:26 +0900

    v1.2

--- iproxy.js
+++ iproxy.js
@@ -1,4 +1,4 @@
-let http        = require('http')
+const http        = require('http')
     , ip        = require('ip')
     , moment    = require('moment')
     , url       = require('url')
@@ -7,18 +7,25 @@ let http        = require('http')
     , env       = require('dotenv').config()
     , cluster   = require('cluster')
     , os        = require('os')
+    , colors    = require('colors/safe')
     , workers   = [];
 
 const HOST_IP = ip.address();
 const LISTEN_PORT = 8722;
-const VERSION = '1.0';
+const VERSION = '1.2';
 const REGEX_EXCEPT_DOMAINS = /^(?:(?:(?!adn|ads|static|upload|upload2|kstatic|img|fnt|pds)[\w\d\-]+)\.)?inven\.co\.kr$/i;
+
+const fReformPath = (path) => {
+    path = String(path).replace(/^https?\:\/\/(?:www\.)?inven\.co\.kr/i, '');
+    return path.length >= 1 ? path : '/';
+};
 
 proxy.on('proxyReq', (proxyReq, req, res, options) => {
     let target = url.parse(req.url);
 
     if (REGEX_EXCEPT_DOMAINS.test(target.host)) {
         proxyReq.setHeader('X-Special-Inven-Header', `iProxy@${VERSION} <${process.env.PROXY_USER}>`);
+        console.log(` iProxy@${VERSION} ${colors.yellow(req.method)} ${proxyReq.path} ${colors.cyan(req.connection.remoteAddress)}`);
     }
 });
 
@@ -36,7 +43,6 @@ http.createServer((req, res) => {
 
     if (REGEX_EXCEPT_DOMAINS.test(target.host)) {
         target.host = process.env.MIRROR_HOST;
-        console.log('[%s] forwarded %s to %s', moment().format('YYYY-MM-DD HH:mm:ss'), req.url, target.host);
     }
 
     proxy.web(req, res, {
